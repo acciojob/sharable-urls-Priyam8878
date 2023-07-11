b850b165b86db11ae7bd7a96ca62a71c3e2e0157
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-07-11 15:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+let newUrl = `https://localhost:8080/?name=${name}&year=${year}`;
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+	document.getElementById('url').innerText = newUrl;
+
+  // Rest of the code goes here
+});
+
